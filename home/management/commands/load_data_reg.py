d9937f833f8beab8b05e5bd690e82ba0f4c3dc5d
robert <rfjohnstonunicef@gmail.com>
2017-01-29 15:27:31 +0100

    separated files for reg and pro data

--- home/management/commands/load_data_reg.py
+++ home/management/commands/load_data_reg.py
@@ -2,8 +2,11 @@ from django.core.management.base import BaseCommand, CommandError
 import pandas as pd
 from sqlalchemy import create_engine
 from django.conf import settings
+from load_data import rename_cols
 
 # to run python manage.py load_data
+
+# remember to delete the table in PostGresSql before running load or the column names do not appear
 
 class Command(BaseCommand):
     help = 'Loads data to SQL for IMAM website'
@@ -13,6 +16,9 @@ class Command(BaseCommand):
 
         # Load Registration dataframe - note for contacts  - use 'Contacts' tab and not 'Runs'
         df = pd.ExcelFile('/home/robert/Downloads/reg.xlsx').parse('Contacts')
+
+        # Rename all the columns in the imported data
+        rename_cols(df)
 
         # Change the order (the index) of the columns
         columnsTitles = ['contact_uuid', 'urn', 'name', 'groups', 'siteid', 'type', 'first_seen', 'last_seen', 'post',
@@ -32,7 +38,7 @@ class Command(BaseCommand):
             with engine.connect() as con:
                 con.execute('ALTER TABLE registration ADD PRIMARY KEY (urn);')
                 # add time zones with same code
-                print("registration data added.")
+                print("Registration data added.")
 
         # KeyboardInterrupt - will likely never return an exception
         except KeyboardInterrupt:
