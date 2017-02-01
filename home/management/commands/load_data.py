4917714e8be7ff0a22d59e494e73c4bd256ac995
robert <rfjohnstonunicef@gmail.com>
2017-02-01 16:30:13 +0100

    complete rename vars function in load data

--- home/management/commands/load_data.py
+++ home/management/commands/load_data.py
@@ -39,9 +39,34 @@ def rename_cols(dataframe):
     # confirm correct data entry
     dataframe.rename(columns={'Confirm (Category) - IMAM Program': 'confirm'}, inplace=True)
 
+
     # Stock database
+    dataframe.rename(columns={'PostLevel (Value) - IMAM Stock ': 'level'}, inplace=True)
+    dataframe.rename(columns={'WeekNum (Value) - IMAM Stock ': 'weeknum'}, inplace=True)
+    dataframe.rename(columns={'Self Report (Value) - IMAM Stock ': 'self_report'}, inplace=True)
+    dataframe.rename(columns={'StoSiteID (Value) - IMAM Stock ': 'sto_siteid'}, inplace=True)
+    dataframe.rename(columns={'StoType (Category) - IMAM Stock ': 'sto_type'}, inplace=True)
+    dataframe.rename(columns={'route_by_type (Category) - IMAM Stock ': 'type'}, inplace=True)
+    dataframe.rename(columns={'RUTF_in (Value) - IMAM Stock ': 'rutf_in'}, inplace=True)
+    # Outpatients
+    dataframe.rename(columns={'RUTF_used_carton (Value) - IMAM Stock ': 'rutf_used_carton'}, inplace=True)
+    dataframe.rename(columns={'RUTF_used_sachet (Value) - IMAM Stock ': 'rutf_used_sachet'}, inplace=True)
+    dataframe.rename(columns={'RUTF_bal_carton (Value) - IMAM Stock ': 'rutf_bal_carton'}, inplace=True)
+    dataframe.rename(columns={'RUTF_bal_sachet (Value) - IMAM Stock ': 'rutf_bal_sachet'}, inplace=True)
+    # Inpatients
+    dataframe.rename(columns={'F75_bal_carton (Value) - IMAM Stock ': 'f75_bal_carton'}, inplace=True)
+    dataframe.rename(columns={'F75_bal_sachet (Value) - IMAM Stock ': 'f75_bal_sachet'}, inplace=True)
+    dataframe.rename(columns={'F100_bal_carton (Value) - IMAM Stock ': 'f100_bal_carton'}, inplace=True)
+    dataframe.rename(columns={'F100_bal_sachet (Value) - IMAM Stock ': 'f100_bal_sachet'}, inplace=True)
+    # confirm correct data entry
+    dataframe.rename(columns={'Confirm (Category) - IMAM Stock ': 'confirm'}, inplace=True)
 
     # LGA database
+    dataframe.rename(columns={'WeekNum (Value) - IMAM LGA State Stocks': 'weeknum'}, inplace=True)
+    dataframe.rename(columns={'RUTF_in (Value) - IMAM LGA State Stocks': 'rutf_in'}, inplace=True)
+    dataframe.rename(columns={'RUTF_out (Value) - IMAM LGA State Stocks': 'rutf_out'}, inplace=True)
+    dataframe.rename(columns={'RUTF_bal (Value) - IMAM LGA State Stocks': 'rutf_bal'}, inplace=True)
+    dataframe.rename(columns={'confirm (Category) - IMAM LGA State Stocks': 'confirm'}, inplace=True)
 
     return dataframe
 
@@ -57,7 +82,7 @@ def merge_in_and_outpatients(dataframe):
     dataframe['dmed'] = np.where(dataframe['type'] == "SC", dataframe['Dmed_i (Value) - IMAM Program'], dataframe['dmed'])
     dataframe['tout'] = np.where(dataframe['type'] == "SC", dataframe['Tout_i (Value) - IMAM Program'], dataframe['tout'])
 
-    return
+    return dataframe
 
 
 def assign_state_lga_num(dataframe):
@@ -68,4 +93,4 @@ def assign_state_lga_num(dataframe):
     dataframe['lga_num'] = dataframe['siteid'].astype(str).str[:4]
     dataframe['lga_num'] = np.where(dataframe['siteid_lgt']==9, dataframe['siteid'].astype(str).str[:3], dataframe['lga_num'])
 
-return dataframe
+    return dataframe
