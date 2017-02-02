039e97c4af972a130172a40678faf261f3fcab57
robert <rfjohnstonunicef@gmail.com>
2017-02-02 13:50:52 +0100

    added all models - functioning version

--- home/models.py
+++ home/models.py
@@ -4,9 +4,8 @@ from django.db import models
 
 
 # Load of initial data done with django custom commands
-# python manage.py load_data
+# use - python manage.py load_data
 
-# Create your models here.
 
 # Registration
 class Registration(models.Model):
@@ -30,131 +29,130 @@ class Registration(models.Model):
 
     def __str__(self):
         return "Registration {}".format(self.name)
-#
-#
-## Program data
-#class Program(models.Model):
-#    contact_uuid = models.UUIDField(editable=False, primary_key=True)
-#    # problem to add phone number field to tools
-#    # urn = models.PhoneNumberField()
-#    urn = models.IntegerField()
-#    name =   models.CharField(max_length=100)
-#    groups = models.CharField(max_length=100)
-#    # if supervisor enters the data than take SiteID from proSiteID
-#    siteid = models.IntegerField()
-#    first_seen = models.DateTimeField()
-#    last_seen =  models.DateTimeField()
-#    weeknum = models.IntegerField()
-#    role = models.CharField(max_length=20)
-#    # if supervisor enters the data then take Type from proType
-#    type = models.CharField(max_length=20)
-#    age_group = models.CharField(max_length=20)
-#    # Admissions and Exits
-#    # if data entry is from SC then don't forget to add
-#    beg =  models.IntegerField()
-#    amar = models.IntegerField()
-#    tin =  models.IntegerField()
-#    tout = models.IntegerField()
-#    dcur = models.IntegerField()
-#    dead = models.IntegerField()
-#    defu = models.IntegerField()
-#    dmed = models.IntegerField()
-#    confirm = models.CharField(max_length=20)
-#
-#    class Meta:
-#        db_table = 'program'
-#        managed = False
-#
-#    def __str__(self):
-#        return "Program {}".format(self.name)
-#
-#
-## Stock data
-#class Stock(models.Model):
-#    contact_uuid = models.UUIDField(editable=False)
-#    # problem to add phone number field to tools
-#    # urn = models.PhoneNumberField()
-#    urn = models.IntegerField(primary_key=True)
-#    name =   models.CharField(max_length=100)
-#    groups = models.CharField(max_length=100)
-#    # if supervisor enters the data than take SiteID from StoSiteID
-#    siteid = models.IntegerField()
-#    first_seen = models.DateTimeField()
-#    last_seen =  models.DateTimeField()
-#    # if supervisor enters the data then don't forget to take data
-#    # level (first, second, site)
-#    level = models.CharField(max_length=20)
-#    weeknum = models.IntegerField()
-#    # if supervisor enters the data then take Type from stoType
-#    type = models.CharField(max_length=20)
-#    rutf_in = models.DecimalField(max_digits=8, decimal_places=2,)
-#    rutf_used_carton = models.DecimalField(max_digits=8, decimal_places=2,)
-#    rutf_used_sachet = models.DecimalField(max_digits=7, decimal_places=2,)
-#    rutf_bal_cartons = models.DecimalField(max_digits=8, decimal_places=2,)
-#    rutf_bal_sachet =  models.DecimalField(max_digits=7, decimal_places=2,)
-#    f75_bal_carton =   models.DecimalField(max_digits=7, decimal_places=2,)
-#    f75_bal_sachet =   models.DecimalField(max_digits=7, decimal_places=2,)
-#    f100_bal_carton =  models.DecimalField(max_digits=7, decimal_places=2,)
-#    f100_bal_sachet =  models.DecimalField(max_digits=7, decimal_places=2,)
-#    confirm = models.CharField(max_length=20)
-#
-#    class Meta:
-#        db_table = 'stock'
-#        managed = False
-#
-#    def __str__(self):
-#        return "Stock {}".format(self.name)
-#
-#
-## State and LGA Stock data
-#class Lga(models.Model):
-#    contact_uuid = models.UUIDField(editable=False)
-#    # problem to add phone number field to tools
-#    # urn = models.PhoneNumberField()
-#    urn = models.IntegerField(primary_key=True)
-#    name =   models.CharField(max_length=100)
-#    groups = models.CharField(max_length=100)
-#    siteid = models.IntegerField()
-#    first_seen = models.DateTimeField('first seen')
-#    last_seen =  models.DateTimeField('last seen')
-#    weeknum = models.IntegerField()
-#    rutf_in =          models.DecimalField(max_digits=8, decimal_places=2,)
-#    rutf_out_carton =  models.DecimalField(max_digits=8, decimal_places=2,)
-#    rutf_bal_cartons = models.DecimalField(max_digits=8, decimal_places=2,)
-#    # can add tracking of f75 and f100 later at LGA level
-#    f75_bal_carton =   models.DecimalField(max_digits=7, decimal_places=2,)
-#    f100_bal_carton =  models.DecimalField(max_digits=7, decimal_places=2,)
-#    confirm = models.CharField(max_length=20)
-#
-#    class Meta:
-#        db_table = 'lga'
-#        managed = False
-#
-#    def __str__(self):
-#        return "Lga {}".format(self.name)
-#
-#
-## Site IDs
-#class Siteid(models.Model):
-#    siteid     = models.IntegerField(primary_key=True)
-#    state      = models.CharField(max_length=100)
-#    state_num  = models.IntegerField()
-#    lga        = models.CharField(max_length=100)
-#    lga_num    = models.IntegerField()
-#    ward       = models.CharField(max_length=100)
-#    sitename   = models.CharField(max_length=120)
-#    x_lat      = models.DecimalField(max_digits=8, decimal_places=1,)
-#    y_long     = models.DecimalField(max_digits=8, decimal_places=1,)
-#    notes      = models.CharField(max_length=300)
-#
-#    class Meta:
-#        db_table = 'siteid'
-#        managed = False
-#
-#    def __str__(self):
-#        return "Siteid {}".format(self.name)
+
+
+# Program data
+class Program(models.Model):
+    contact_uuid = models.UUIDField(editable=False)
+    # problem to add phone number field to tools
+    # urn = models.PhoneNumberField()
+    urn = models.IntegerField()
+    name =   models.CharField(max_length=100)
+    groups = models.CharField(max_length=100)
+    # if supervisor enters the data than take SiteID from proSiteID
+    siteid = models.IntegerField()
+    first_seen = models.DateTimeField()
+    last_seen =  models.DateTimeField()
+    weeknum = models.IntegerField()
+    role = models.CharField(max_length=20)
+    # if supervisor enters the data then take Type from proType
+    type = models.CharField(max_length=20)
+    age_group = models.CharField(max_length=20)
+    # Admissions and Exits
+    # if data entry is from SC then don't forget to add
+    beg =  models.IntegerField()
+    amar = models.IntegerField()
+    tin =  models.IntegerField()
+    tout = models.IntegerField()
+    dcur = models.IntegerField()
+    dead = models.IntegerField()
+    defu = models.IntegerField()
+    dmed = models.IntegerField()
+    confirm = models.CharField(max_length=20)
+
+    class Meta:
+        db_table = 'program'
+        managed = False
+
+    def __str__(self):
+        return "Program {}".format(self.name)
+
+
+# Stock data
+class Stock(models.Model):
+    contact_uuid = models.UUIDField(editable=False)
+    # problem to add phone number field to tools
+    # urn = models.PhoneNumberField()
+    urn = models.IntegerField(primary_key=True)
+    name =   models.CharField(max_length=100)
+    groups = models.CharField(max_length=100)
+    # if supervisor enters the data than take SiteID from StoSiteID
+    siteid = models.IntegerField()
+    first_seen = models.DateTimeField()
+    last_seen =  models.DateTimeField()
+    # if supervisor enters the data then don't forget to take data
+    # level (first, second, site)
+    level = models.CharField(max_length=20)
+    weeknum = models.IntegerField()
+    # if supervisor enters the data then take Type from stoType
+    type = models.CharField(max_length=20)
+    rutf_in = models.DecimalField(max_digits=8, decimal_places=2,)
+    rutf_used_carton = models.DecimalField(max_digits=8, decimal_places=2,)
+    rutf_used_sachet = models.DecimalField(max_digits=7, decimal_places=2,)
+    rutf_bal_cartons = models.DecimalField(max_digits=8, decimal_places=2,)
+    rutf_bal_sachet =  models.DecimalField(max_digits=7, decimal_places=2,)
+    f75_bal_carton =   models.DecimalField(max_digits=7, decimal_places=2,)
+    f75_bal_sachet =   models.DecimalField(max_digits=7, decimal_places=2,)
+    f100_bal_carton =  models.DecimalField(max_digits=7, decimal_places=2,)
+    f100_bal_sachet =  models.DecimalField(max_digits=7, decimal_places=2,)
+    confirm = models.CharField(max_length=20)
+
+    class Meta:
+        db_table = 'stock'
+        managed = False
+
+    def __str__(self):
+        return "Stock {}".format(self.name)
+
+
+# State and LGA Stock data
+class Lga(models.Model):
+    contact_uuid = models.UUIDField(editable=False)
+    # problem to add phone number field to tools
+    # urn = models.PhoneNumberField()
+    urn = models.IntegerField(primary_key=True)
+    name =   models.CharField(max_length=100)
+    groups = models.CharField(max_length=100)
+    siteid = models.IntegerField()
+    first_seen = models.DateTimeField('first seen')
+    last_seen =  models.DateTimeField('last seen')
+    weeknum = models.IntegerField()
+    rutf_in =          models.DecimalField(max_digits=8, decimal_places=2,)
+    rutf_out_carton =  models.DecimalField(max_digits=8, decimal_places=2,)
+    rutf_bal_cartons = models.DecimalField(max_digits=8, decimal_places=2,)
+    # can add tracking of f75 and f100 later at LGA level
+    f75_bal_carton =   models.DecimalField(max_digits=7, decimal_places=2,)
+    f100_bal_carton =  models.DecimalField(max_digits=7, decimal_places=2,)
+    confirm = models.CharField(max_length=20)
+
+    class Meta:
+        db_table = 'lga'
+        managed = False
+
+    def __str__(self):
+        return "Lga {}".format(self.name)
+
+
+# Site IDs
+class Siteid(models.Model):
+    siteid     = models.IntegerField(primary_key=True)
+    state      = models.CharField(max_length=100)
+    state_num  = models.IntegerField()
+    lga        = models.CharField(max_length=100)
+    lga_num    = models.IntegerField()
+    ward       = models.CharField(max_length=100)
+    sitename   = models.CharField(max_length=120)
+    x_lat      = models.DecimalField(max_digits=8, decimal_places=1,)
+    y_long     = models.DecimalField(max_digits=8, decimal_places=1,)
+    notes      = models.CharField(max_length=300)
+
+    class Meta:
+        db_table = 'siteid'
+        managed = False
+
+    def __str__(self):
+        return "Siteid {}".format(self.name)
 
 
 # how do we load the raw data once and then start to use the api after ?
 # this will be important as the database size increases
-
--- home/views.py
+++ home/views.py
@@ -2,20 +2,37 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.conf import settings
 import pandas as pd
+import numpy as np
+import pandas_highcharts.core
 from sqlalchemy import create_engine
 
 # Read data into dataframe
-engine = create_engine('postgresql://{USER}:{PASSWORD}@{HOST}:{PORT}/{NAME}'.format(**settings.DATABASES['default']))
-df = pd.read_sql_query("select * from registration;", con=engine)
 
-# create table to show in HTML
-crosstab = pd.crosstab(df['post'],df['type'],margins=True)
-html_table = crosstab.to_html(index=False)
+# This is the problem that crashes project.
+
+engine = create_engine('postgresql://{USER}:{PASSWORD}@{HOST}:{PORT}/{NAME}'.format(**settings.DATABASES['default']))
+df = pd.read_sql_query("select * from program;", con=engine)
+
+# fake data in data frame to use
+# df = pd.DataFrame({'amar': np.random.randn(5)*100})
+
+
+def index(request):
+
+    # adm_by_week = df['amar'].groupby(df['weeknum']).mean()
+    adm_by_week = df
+    chart = pandas_highcharts.core.serialize(adm_by_week, render_to='my-chart',output_type='json')
+    # module object has no attribute serialize unless you specify pandas except with pandas_highcharts.core
+
+    return render(request, 'home/index.html')
+
+# USING HTML.to_html
 
 #def index(request):
 #    return render_to_response('home/index.html', {'html.table': html_table}, RequestContext(request))
 
-# Create your views here.
-def index(request):
-     return render(request, 'home/index.html')
+# create table to show in HTML
+#crosstab = pd.crosstab(df['post'],df['type'],margins=True)
+#html_table = crosstab.to_html(index=False)
+
 
