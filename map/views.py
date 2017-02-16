25bc8f8bf166aacd5c4c82115bd1f972f3e505f9
robert <rfjohnstonunicef@gmail.com>
2017-02-16 18:44:54 +0100

    added date to admissions graph

--- map/views.py
+++ map/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+import time
+
 # Create your views here.
 def map(request):
 
-    adm_dict = {'garbage': "Pass the vulnuisbak, ASB"}
+    adm_dict = {'date': time.strftime("%d/%m/%y")}
 
     return render(request, 'map/map.html', context=adm_dict )
     #return render(request, 'map.html', {'content': ['Insert Map here']})
+    # after the template dir add the map dir to correctly identify the path
 
 
