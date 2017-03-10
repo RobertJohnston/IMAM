7069a5ce28ba7dc6323032c524b17061dc8f5a79
robert <rfjohnstonunicef@gmail.com>
2017-03-10 19:38:22 +0100

    corrected x-axis to have 2016/17 data

--- home/views.py
+++ home/views.py
@@ -7,22 +7,43 @@ import pandas as pd
 import time
 import numpy as np
 
+from datetime import date, timedelta
+
 from sqlalchemy import create_engine
 from management.commands.load_data import assign_state_lga_num
 
 from models import First_admin, Second_admin, Site
 
 
-def rate_by_week(df_filtered, kind, num):
-    adm_by_week = df_filtered.query('%s==%s' % (kind, num))['amar'].groupby(df_filtered['weeknum']).sum()
+def iso_year_start(iso_year):
+    "The gregorian calendar date of the first day of the given ISO year"
+    fourth_jan = date(iso_year, 1, 4)
+    delta = timedelta(fourth_jan.isoweekday() - 1)
+    return fourth_jan - delta
 
-    filter_discharge = df_filtered.query('%s==%s' % (kind, num))['total_discharges'].groupby(df_filtered['weeknum']).sum()
-    filter_cout = df_filtered.query('%s==%s' % (kind, num))['cout'].groupby(df_filtered['weeknum']).sum()
 
-    dead_rate_by_week = df_filtered.query('%s==%s' % (kind, num))['dead'].groupby(df_filtered['weeknum']).sum() / filter_discharge * 100
-    defu_rate_by_week = df_filtered.query('%s==%s' % (kind, num))['defu'].groupby(df_filtered['weeknum']).sum() / filter_discharge * 100
-    dmed_rate_by_week = df_filtered.query('%s==%s' % (kind, num))['dmed'].groupby(df_filtered['weeknum']).sum() / filter_discharge * 100
-    tout_rate_by_week = df_filtered.query('%s==%s' % (kind, num))['tout'].groupby(df_filtered['weeknum']).sum() / filter_cout * 100
+def iso_to_gregorian(iso_year, iso_week, iso_day=1):
+    "Gregorian calendar date for the given ISO year, week and day"
+    year_start = iso_year_start(iso_year)
+    return int((year_start + timedelta(days=iso_day - 1, weeks=iso_week - 1)).strftime('%s') + '000')
+
+
+def rate_by_week(df_filtered, kind=None, num=None):
+    # this is nationnal level, no need for query
+    if kind is None:
+        df_queried = df_filtered
+    else:
+        df_queried = df_filtered.query('%s==%s' % (kind, num))
+
+    adm_by_week = df_queried['amar'].groupby([df_filtered['year'], df_filtered['weeknum']]).sum()
+
+    filter_discharge = df_queried['total_discharges'].groupby([df_filtered['year'], df_filtered['weeknum']]).sum()
+    filter_cout = df_queried['cout'].groupby([df_filtered['year'], df_filtered['weeknum']]).sum()
+
+    dead_rate_by_week = df_queried['dead'].groupby([df_filtered['year'], df_filtered['weeknum']]).sum() / filter_discharge * 100
+    defu_rate_by_week = df_queried['defu'].groupby([df_filtered['year'], df_filtered['weeknum']]).sum() / filter_discharge * 100
+    dmed_rate_by_week = df_queried['dmed'].groupby([df_filtered['year'], df_filtered['weeknum']]).sum() / filter_discharge * 100
+    tout_rate_by_week = df_queried['tout'].groupby([df_filtered['year'], df_filtered['weeknum']]).sum() / filter_cout * 100
 
     dead_rate_by_week = dead_rate_by_week.dropna()
     defu_rate_by_week = defu_rate_by_week.dropna()
@@ -60,11 +81,14 @@ def adm(request):
         df_filtered[i] = pd.to_numeric(df_filtered[i], errors='coerce')
 
     # Clean out of range identification data
-    for i in ('weeknum', 'state_num', 'lga_num', 'siteid', 'amar', 'dcur', 'dead', 'defu', 'dmed', 'tout'):
+    for i in ('weeknum', 'state_num', 'lga_num', 'siteid'):
         df_filtered = df_filtered.query('%s==%s' % (i, i)).query('%s>=0' % i)
-        # line below deletes entire row where a NaN is found
-        # THIS IS ERROR IN DATA CLEANING - should leave in NaN
-        # This does not remove all NaN ???
+        # line above deletes entire row where a NaN is found
+
+    # the change below changes the integrity of data - do not export.
+    for i in ( 'amar', 'dcur', 'dead', 'defu', 'dmed', 'tout'):
+        df_filtered[i] = df_filtered[i].fillna(0)
+
 
     # It is appropriate to delete the entire row of data if there is no ID or week number
     # line below deletes entire row where a NaN is found - see all queries
@@ -80,12 +104,23 @@ def adm(request):
     for i in ('weeknum', 'state_num', 'lga_num', 'siteid', 'amar', 'dcur', 'dead', 'defu', 'dmed', 'tout'):
         df_filtered[i] = df_filtered[i].astype(int)
 
-    # For all exit rate calculations see Final Report Consensus Meeting on M&E IMAM December 2010
+    # Introducing Year for X axis
+    df_filtered['year'] = df_filtered['last_seen'].map(lambda x: x.year)
+    # double check if the week number below is ISO standard
+    df_filtered['last_seen_weeknum'] = df_filtered['last_seen'].map(lambda x: x.week)
 
-    #####
-    # Exit rates at national level are not correct
-    # other exit rates at lower level are not correct as denominator is only at national level.
-    #####
+    df_filtered['year'] = np.where(df_filtered['last_seen_weeknum'] < df_filtered['weeknum'],
+                                        df_filtered['year'] - 1, df_filtered['year'])
+
+    today_year = date.today().year
+    today_weeknum = date.today().isocalendar()[1]
+
+    # df_filtered = df_filtered.sort_values(['year', 'weeknum'])\
+    df_filtered = df_filtered\
+        [(df_filtered['year'] >= 2017) | ((df_filtered['year'] == 2016) & (df_filtered['weeknum'] >= 22))]\
+        [(df_filtered['year'] < today_year) | ((df_filtered['year'] == today_year) & (df_filtered['weeknum'] <= today_weeknum))]
+
+    # For all exit rate calculations see Final Report Consensus Meeting on M&E IMAM December 2010
 
     # Total Discharges from program
     df_filtered['total_discharges'] = df_filtered.dcur + df_filtered.dead + df_filtered.defu + df_filtered.dmed
@@ -99,11 +134,7 @@ def adm(request):
     if "site_filter" not in request.GET or request.GET['site_filter'] == "":
         adm_by_week = df_filtered['amar'].groupby(df_filtered['weeknum']).sum()
 
-        # This method is almost correct - should be sum of deaths by week over total discharges by week *100
-        dead_rate_by_week = df_filtered['dead'].groupby(df_filtered['weeknum']).sum() / df_filtered['total_discharges'].groupby(df_filtered['weeknum']).sum() *100
-        defu_rate_by_week = df_filtered['defu'].groupby(df_filtered['weeknum']).sum() / df_filtered['total_discharges'].groupby(df_filtered['weeknum']).sum() *100
-        dmed_rate_by_week = df_filtered['dmed'].groupby(df_filtered['weeknum']).sum() / df_filtered['total_discharges'].groupby(df_filtered['weeknum']).sum() *100
-        tout_rate_by_week = df_filtered['tout'].groupby(df_filtered['weeknum']).sum() / df_filtered['cout'].groupby(df_filtered['weeknum']).sum() *100
+        adm_by_week, dead_rate_by_week, defu_rate_by_week, dmed_rate_by_week, tout_rate_by_week = rate_by_week(df_filtered)
 
         title = "National Level"
 
@@ -147,14 +178,14 @@ def adm(request):
         else:
             raise Exception("We have encountered a datatype that we don't know how to handle: %s" % data_type)
 
-    adm_by_week = list(zip(adm_by_week.index, adm_by_week.values.tolist()))
+    adm_by_week = list(zip([iso_to_gregorian(x[0], x[1]) for x in adm_by_week.index], adm_by_week.values.tolist()))
 
-    dead_rate_by_week = list(zip(dead_rate_by_week.index, dead_rate_by_week.values.tolist()))
-    defu_rate_by_week = list(zip(defu_rate_by_week.index, defu_rate_by_week.values.tolist()))
-    dmed_rate_by_week = list(zip(dmed_rate_by_week.index, dmed_rate_by_week.values.tolist()))
-    tout_rate_by_week = list(zip(tout_rate_by_week.index, tout_rate_by_week.values.tolist()))
+    dead_rate_by_week = list(zip([iso_to_gregorian(x[0], x[1]) for x in dead_rate_by_week.index], dead_rate_by_week.values.tolist()))
+    defu_rate_by_week = list(zip([iso_to_gregorian(x[0], x[1]) for x in defu_rate_by_week.index], defu_rate_by_week.values.tolist()))
+    dmed_rate_by_week = list(zip([iso_to_gregorian(x[0], x[1]) for x in dmed_rate_by_week.index], dmed_rate_by_week.values.tolist()))
+    tout_rate_by_week = list(zip([iso_to_gregorian(x[0], x[1]) for x in tout_rate_by_week.index], tout_rate_by_week.values.tolist()))
 
-    date = {'date': time.strftime("%d/%m/%y")}
+    # date = {'date': time.strftime("%d/%m/%y")}
 
     # return HttpResponse(json.dumps(result)
 
@@ -165,7 +196,7 @@ def adm(request):
         "dmed_rate_by_week": dmed_rate_by_week,
         "tout_rate_by_week": tout_rate_by_week,
         "title": title,
-        # "date": date,
+        "date": date.today().strftime("%d/%m/%Y"),
         #   Cannot pass data through because of json dumps?
     }))
 
