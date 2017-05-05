de443dff311c6e24724d5699a505eccc7bbc7493
robert <rfjohnstonunicef@gmail.com>
2017-05-05 16:50:25 +0200

    makes stock importation works

--- home/management/commands/import_stock.py
+++ home/management/commands/import_stock.py
@@ -1,11 +1,11 @@
-from datetime import date
+from datetime import date, datetime
 
 from django.core.management.base import BaseCommand
 from django.db import transaction
 
 from temba_client.v2 import TembaClient
 from isoweek import Week
-from home.models import Stock, Registration
+from home.models import Stock, Registration, LastUpdatedAPICall
 
 from uuid import UUID
 
@@ -22,12 +22,22 @@ class Command(BaseCommand):
 
         contact_cache = {x.contact_uuid: x for x in Registration.objects.all()}
 
+        last_update_time = LastUpdatedAPICall.objects.filter(kind="stock").first()
+
+        if last_update_time:
+            clients_from_api = client.get_runs(flow=u'a678268d-0e42-43f1-82cd-aa12117d145d', after=last_update_time.timestamp)
+        else:
+            clients_from_api = client.get_runs(flow=u'a678268d-0e42-43f1-82cd-aa12117d145d')
+            last_update_time = LastUpdatedAPICall(kind="stock")
+
+        last_update_time.timestamp = datetime.now()
+
         a = 0
         # rapidpro expects a uuid to identify flow instead of a flow name
         # https://app.rapidpro.io/flow/editor/a678268d-0e42-43f1-82cd-aa12117d145d/
-        for stock_batch in client.get_runs(flow=u'a678268d-0e42-43f1-82cd-aa12117d145d').iterfetches(retry_on_rate_exceed=True):
+        for stock_batch in clients_from_api.iterfetches(retry_on_rate_exceed=True):
 
-            # with transaction.atomic():
+            with transaction.atomic():
             # Optimization tool - transaction.atomic -is turned off here as it hides errors.
 
                 # the API response is a list in a list
@@ -35,6 +45,10 @@ class Command(BaseCommand):
                 for stock in stock_batch:
                     if 'confirm' not in stock.values or stock.values['confirm'].category != 'Yes':
                         print('     Unconfirmed Entry')
+                        continue
+
+                    if 'weeknum' not in stock.values:
+                        print('     Entry without a weeknum, skip it')
                         continue
 
                     # if id of stock exists then update the row
@@ -51,8 +65,51 @@ class Command(BaseCommand):
                     stock_in_db.urn = contact.urn
                     stock_in_db.name = contact.name
 
+                    stock_in_db.siteid = contact.siteid
+
+                    stock_in_db.weeknum = stock.values['weeknum'].value
+
                     stock_in_db.first_seen = stock.created_on
                     stock_in_db.last_seen =  stock.modified_on
+
+                    # implementation
+                    if 'route_by_type' in stock.values:
+                        stock_in_db.type = stock.values['route_by_type'].category
+                    # supervision
+                    elif 'route_by_level' in stock.values:
+                        stock_in_db.type = stock.values['stotype'].category
+                        stock_in_db.siteid = stock.values['stositeid'].value
+                    else:
+                        raise Exception()
+
+
+                    if stock_in_db.type == 'OTP':
+                        stock_in_db.rutf_in = stock.values['rutf_in'].value
+                        stock_in_db.rutf_used_carton = stock.values['rutf_used_carton'].value
+                        stock_in_db.rutf_used_sachet = stock.values['rutf_used_sachet'].value
+                        stock_in_db.rutf_bal_carton = stock.values['rutf_bal_carton'].value
+                        stock_in_db.rutf_bal_sachet = stock.values['rutf_bal_sachet'].value
+                    elif stock_in_db.type == 'SC':
+                        # for stabilization center only balance
+                        stock_in_db.f100_bal_carton = stock.values['f100_bal_carton'].value
+                        stock_in_db.f100_bal_sachet = stock.values['f100_bal_sachet'].value
+                        stock_in_db.f75_bal_carton = stock.values['f75_bal_carton'].value
+                        stock_in_db.f75_bal_sachet = stock.values['f75_bal_sachet'].value
+                    else:
+                        raise Exception()
+
+
+
+
+                    # # Introducing Year for X axis
+                    stock_in_db.year = stock_in_db.last_seen.isocalendar()[0]
+                    last_seen_weeknum = stock_in_db.last_seen.isocalendar()[1]
+
+                    # If report was for weeknum in last year but report data is this year, subtract one year from dataframe.year.
+                    # this double conditional identifies the year correctly in the majority of cases
+                    # except for reports with weeknum <=44 and more than 8 weeks in past
+                    if stock_in_db.weeknum > 44 and last_seen_weeknum < stock_in_db.weeknum:
+                        stock_in_db.year -= 1
 
                     stock_in_db.save()  # Drop duplicates
 
@@ -68,3 +125,5 @@ class Command(BaseCommand):
             
                     a += 1
                     print(a)
+
+        last_update_time.save()
