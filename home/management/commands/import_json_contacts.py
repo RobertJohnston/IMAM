827417e19fc3900f0d68b8037a5e51bcef52dfa9
robert <rfjohnstonunicef@gmail.com>
2017-05-31 06:49:04 +0200

    import json for contacts

---
+++ home/management/commands/import_json_contacts.py
@@ -0,0 +1,75 @@
+import json
+import requests
+from urllib import quote
+from datetime import datetime
+
+from django.core.management.base import BaseCommand
+from django.db import transaction
+from home.models import JsonRegistration, LastUpdatedAPICall
+
+# to run python manage.py (name of file), OR
+# ipython --pdb manage.py (name of file)
+
+
+class Command(BaseCommand):
+    help = 'Loads registration data to SQL through API'
+
+    # def add_arguments(self, parser):
+    #     parser.add_argument(
+    #         '--all',
+    #         action='store_true',
+    #         dest='all',
+    #         default=False,
+    #         help='Import all data',
+    #     )
+
+    # A command must define handle
+    def handle(self, *args, **options):
+        token = open('token').read().strip()
+        api_base_url = "https://rapidpro.io"
+
+        # Check in PostGreSQL database for last timestamp of API call
+        # last_update_time = LastUpdatedAPICall.objects.filter(kind="contact").first()
+        #
+        # Code below is explicitly describing all possible four conditions.
+        # T/T   T/F    F/T    F/F
+        # if options['all'] and last_update_time:
+        #     clients_from_api = client.get_contacts(group='Nut Personnel')
+        #
+        # elif options['all'] and not last_update_time:
+        #     clients_from_api = client.get_contacts(group='Nut Personnel')
+        #     last_update_time = LastUpdatedAPICall(kind="contact")
+        #
+        # elif not options['all'] and last_update_time:
+        #     clients_from_api = client.get_contacts(group='Nut Personnel', after=last_update_time.timestamp)
+        #
+        # elif not options['all'] and not last_update_time:
+        #     clients_from_api = client.get_contacts(group='Nut Personnel')
+        #     last_update_time = LastUpdatedAPICall(kind="contact")
+        #
+        # last_update_time.timestamp = datetime.now()
+        #
+        # a = 0
+
+        page_url_to_process = api_base_url + "/api/v2/contacts.json?"
+
+        while page_url_to_process:
+            response = requests.get(page_url_to_process + "&group=%s" % quote('Nut Personnel'), headers={"Authorization": "Token %s" % token})
+            # Must include Nut Personnel in ?Query to filter for personnel of IMAM program
+
+            assert response.status_code == 200, "%s: %s" % (response.status_code, response.content)
+
+            for api_contact in response.json()['results']:
+                uuid = api_contact['uuid']
+                print(uuid)
+
+                if JsonRegistration.objects.filter(uuid=uuid):
+                    in_db_contact = JsonRegistration.objects.get(uuid=uuid)
+                else:
+                    in_db_contact = JsonRegistration()
+                    in_db_contact.uuid = uuid
+
+                in_db_contact.json = json.dumps(api_contact, indent=4)
+                in_db_contact.save()
+
+            page_url_to_process = response.json()['next']
---
+++ home/utilities.py
@@ -0,0 +1,20 @@
+# utilities to use in views.py
+
+def iso_year_start(iso_year):
+    "The gregorian calendar date of the first day of the given ISO year"
+    fourth_jan = date(iso_year, 1, 4)
+    delta = timedelta(fourth_jan.isoweekday() - 1)
+    return fourth_jan - delta
+
+
+# Beware of week 52 of 2016 when presented as 2017 (first week of year)
+def iso_to_gregorian(iso_year, iso_week, iso_day=1):
+    "Gregorian calendar date for the given ISO year, week and day"
+    year_start = iso_year_start(iso_year)
+    return int((year_start + timedelta(days=iso_day - 1, weeks=iso_week - 1)).strftime('%s') + '000')
+
+
+# Code to calculate the last weeknum for any given year (some years have 52 others have 53)
+def weeks_for_year(year):
+    last_week = date(year, 12, 28)
+    return last_week.isocalendar()[1]
