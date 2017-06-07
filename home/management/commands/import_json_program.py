7bb62cba0bd92278fa8c79b114673827b9830c23
robert <rfjohnstonunicef@gmail.com>
2017-06-07 18:30:22 +0200

    added --all fcn as Django command

--- home/management/commands/import_json_program.py
+++ home/management/commands/import_json_program.py
@@ -4,22 +4,58 @@ import requests
 from django.core.management.base import BaseCommand
 from django.db import transaction
 from home.models import JsonProgram, LastUpdatedAPICall
-
-program_uuid = "a9eed2f3-a92c-48dd-aa10-4f139b1171a4"
+from datetime import datetime
+from django.conf import settings
 
 class Command(BaseCommand):
     help = 'Imports program data into JSON format from API'
 
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--all',
+            action='store_true',
+            dest='all',
+            default=False,
+            help='Import all data',
+        )
+
     # A command must define handle
     def handle(self, *args, **options):
-        token = open('token').read().strip()
-        api_base_url = "https://rapidpro.io"
+        with transaction.atomic():
 
-        page_url_to_process = api_base_url + "/api/v2/runs.json?flow=%s" % program_uuid
-        a = 0
+            token = open('token').read().strip()
+            api_base_url = "https://rapidpro.io"
 
-        while page_url_to_process:
-            with transaction.atomic():
+            last_update_time = LastUpdatedAPICall.objects.filter(kind="json_program").first()
+
+            # Code below is explicitly describing all possible four conditions.
+            # T/T   T/F    F/T    F/F
+            if options['all'] and last_update_time:
+                page_url_to_process = api_base_url + "/api/v2/runs.json?flow=%s" % settings.PROGRAM_UUID
+                JsonProgram.objects.all().delete()
+
+            elif options['all'] and not last_update_time:
+                page_url_to_process = api_base_url + "/api/v2/runs.json?flow=%s" % settings.PROGRAM_UUID
+                JsonProgram.objects.all().delete()
+                last_update_time = LastUpdatedAPICall(kind="json_program")
+
+            elif not options['all'] and last_update_time:
+                # Start from end of last api call: GET all data since timestamp of last API call
+                page_url_to_process = api_base_url + "/api/v2/runs.json?flow=%s&after=%s" % (
+                    settings.PROGRAM_UUID,
+                    last_update_time.timestamp.strftime("%FT%X.000"),
+                )
+
+            elif not options['all'] and not last_update_time:
+                page_url_to_process = api_base_url + "/api/v2/runs.json?flow=%s" % settings.PROGRAM_UUID
+                JsonProgram.objects.all().delete()
+                last_update_time = LastUpdatedAPICall(kind="json_program")
+
+            last_update_time.timestamp = datetime.now()
+
+            a = 0
+
+            while page_url_to_process:
 
                 response = requests.get(page_url_to_process, headers={"Authorization": "Token %s" % token})
                 assert response.status_code == 200, "%s: %s" % (response.status_code, response.content)
@@ -35,11 +71,14 @@ class Command(BaseCommand):
                         in_db_program = JsonProgram()
                         in_db_program.id = id
 
+                    if in_db_program.json == json.dumps(api_program, indent=4):
+                        continue
+
                     in_db_program.json = json.dumps(api_program, indent=4)
                     in_db_program.save()
 
                 page_url_to_process = response.json()['next']
 
 
-
+        last_update_time.save()
 
