0cec8e9145f4aff3680b4ee878af52d0aef49e9b
robert <rfjohnstonunicef@gmail.com>
2017-06-10 08:26:42 +0200

    add import --all option to import_json_stock

--- home/management/commands/import_json_stock.py
+++ home/management/commands/import_json_stock.py
@@ -4,23 +4,62 @@ import requests
 from django.core.management.base import BaseCommand
 from django.db import transaction
 from home.models import JsonStock, LastUpdatedAPICall
+from datetime import datetime
+from django.conf import settings
 
 # Flow uuid from RapidPro
-stock_uuid = "a678268d-0e42-43f1-82cd-aa12117d145d"
+# stock_uuid = "a678268d-0e42-43f1-82cd-aa12117d145d"
 
 class Command(BaseCommand):
-    help = 'Loads stock data to SQL through API'
+    help = 'Loads stock data into JSON format from API'
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--all',
+            action='store_true',
+            dest='all',
+            default=False,
+            help='Import all data',
+        )
 
     # A command must define handle
     def handle(self, *args, **options):
-        token = open('token').read().strip()
-        api_base_url = "https://rapidpro.io"
+        with transaction.atomic():
 
-        page_url_to_process = api_base_url + "/api/v2/runs.json?flow=%s" % stock_uuid
-        a = 0
+            token = open('token').read().strip()
+            # api_base_url = "https://rapidpro.io"
+            api_base_url = settings.API_BASE_URL
 
-        while page_url_to_process:
-            with transaction.atomic():
+            last_update_time = LastUpdatedAPICall.objects.filter(kind="json_stock").first()
+
+            # Code below is explicitly describing all possible four conditions.
+            if options['all'] and last_update_time:
+                page_url_to_process = api_base_url + "/api/v2/runs.json?flow=%s" % settings.STOCK_UUID
+                JsonStock.objects.all().delete()
+
+            elif options['all'] and not last_update_time:
+                page_url_to_process = api_base_url + "/api/v2/runs.json?flow=%s" % settings.STOCK_UUID
+                JsonStock.objects.all().delete()
+                last_update_time = LastUpdatedAPICall(kind="json_stock")
+
+            elif not options['all'] and last_update_time:
+                # Start from end of last api call: GET all data since timestamp of last API call
+                page_url_to_process = api_base_url + "/api/v2/runs.json?flow=%s&after=%s" % (
+                    settings.STOCK_UUID,
+                    # pass timestamp to RapidPro in time format with T between date and time
+                    last_update_time.timestamp.strftime("%FT%X.000"),
+                )
+
+            elif not options['all'] and not last_update_time:
+                page_url_to_process = api_base_url + "/api/v2/runs.json?flow=%s" % settings.STOCK_UUID
+                JsonStock.objects.all().delete()
+                last_update_time = LastUpdatedAPICall(kind="json_stock")
+
+            last_update_time.timestamp = datetime.now()
+
+            a = 0
+
+            while page_url_to_process:
 
                 response = requests.get(page_url_to_process, headers={"Authorization": "Token %s" % token})
 
@@ -37,12 +76,13 @@ class Command(BaseCommand):
                         in_db_stock = JsonStock()
                         in_db_stock.id = id
 
+                    # if the data in api_stock matches the data in postgres, then skip
+                    if in_db_stock.json == json.dumps(api_stock, indent=4):
+                        continue
+
                     in_db_stock.json = json.dumps(api_stock, indent=4)
                     in_db_stock.save()
 
                 page_url_to_process = response.json()['next']
-                # print(page_url_to_process)
 
-
-
-
+        last_update_time.save()
