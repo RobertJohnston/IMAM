cfa9e5c2043de15f94926dbe29d54680c6016b49
robert <rfjohnstonunicef@gmail.com>
2017-08-20 11:58:33 +0530

    added postgres_stats and url for forms

--- IMAM/settings.py
+++ IMAM/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'raven.contrib.django.raven_compat',
     'home.apps.HomeConfig',
     'map',
+    'postgres_stats',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
